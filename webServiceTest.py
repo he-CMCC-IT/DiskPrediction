8096990c9969e6fd5da3757c4f7adc270bffb1c4
suntengfei <suntengfei@he.chinamobile.com>
2018-11-20 18:34:04 +0800

    调用webService demo,供参考

--- webServiceTest.py
+++ webServiceTest.py
@@ -10,3 +10,14 @@ client= Client(url,doctor=d)
 result= client.service.getWeatherbyCityName("杭州")
 print(result)
 print(datetime.datetime.now())
+
+'''
+第二个实例
+'''
+# imp= Import('http://www.w3.org/2001/XMLSchema', location='http://www.w3.org/2001/XMLSchema.xsd')#导入
+# imp.filter.add("http://WebXml.com.cn/")#<em>namespace如下图</em>
+# d= ImportDoctor(imp)
+# url = "http://fy.webxml.com.cn/webservices/EnglishChinese.asmx?wsdl"
+# client= Client(url,doctor=d)
+# result = client.service.TranslatorReferString("字母")
+# print(result)
